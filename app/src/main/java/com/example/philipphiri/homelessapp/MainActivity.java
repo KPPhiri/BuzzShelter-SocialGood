e056228206c4af31eb2892b8305d88a9eede5ec2
kpphiri <kphiri6@gatech.edu>
2018-02-10 13:50:00 -0500

    Adding login fields

--- app/src/main/java/com/example/philipphiri/homelessapp/MainActivity.java
+++ app/src/main/java/com/example/philipphiri/homelessapp/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.philipphiri.homelessapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); //Testing
+        setContentView(R.layout.activity_main);
+
+        Button login = (Button) findViewById(R.id.loginButton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText logField = (EditText) findViewById(R.id.login);
+                EditText passField = (EditText) findViewById(R.id.password);
+
+                logField.setVisibility(View.VISIBLE);
+                passField.setVisibility(View.VISIBLE);
+            }
+        });
+
+
     }
+
+    
 }
