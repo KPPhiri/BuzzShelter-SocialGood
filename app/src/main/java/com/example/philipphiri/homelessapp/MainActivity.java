29b0955ef0344e069501c53dc1f6142d6fb1eb77
ya2189 <yahn41@gatech.edu>
2018-02-12 23:58:49 -0500

    Deleted redundant code and fixed warnings

--- app/src/main/java/com/example/philipphiri/homelessapp/MainActivity.java
+++ app/src/main/java/com/example/philipphiri/homelessapp/MainActivity.java
@@ -24,45 +24,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        final Button login = (Button) findViewById(R.id.loginButton);
+        final Button login = findViewById(R.id.loginButton);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final EditText logField = (EditText) findViewById(R.id.login);
-                final EditText passField = (EditText) findViewById(R.id.password);
-                final Button cancel = (Button) findViewById(R.id.cancelButton);
+                final EditText logField = findViewById(R.id.login);
+                final EditText passField = findViewById(R.id.password);
+                final Button cancel = findViewById(R.id.cancelButton);
 
                 login.setOnClickListener(
                         new View.OnClickListener() {
                             public void onClick(View view) {
                                 if (logField.getText().toString().equals("user")
                                         && passField.getText().toString().equals("pass")) {
-                                    Log.i("clicks","Success");
+                                    Log.i("MainActivity","Successfully logged in");
                                     Intent i = new Intent(MainActivity.this, Main2Activity.class);
                                     startActivity(i);
                                 } else {
-
-                                    // inflate the layout of the popup window
-                                    LayoutInflater inflater = (LayoutInflater)
-                                            getSystemService(LAYOUT_INFLATER_SERVICE);
+                                    LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
                                     View popupView = inflater.inflate(R.layout.wrong_login, null);
 
-                                    // create the popup window
                                     int width = LinearLayout.LayoutParams.WRAP_CONTENT;
                                     int height = LinearLayout.LayoutParams.WRAP_CONTENT;
-                                    boolean focusable = true; // lets taps outside the popup also dismiss it
-                                    final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+                                    final PopupWindow popupWindow = new PopupWindow(popupView, width, height, true);
 
-                                    // show the popup window
                                     ConstraintLayout activity_main_layout;
                                     activity_main_layout = findViewById(R.id.main_activity_layout);
 
                                     popupWindow.showAtLocation(activity_main_layout, Gravity.CENTER, 0, 0);
 
-                                    // dismiss the popup window when touched
                                     popupView.setOnTouchListener(new View.OnTouchListener() {
                                         @Override
-                                        public boolean onTouch(View v, MotionEvent event) {
+                                        public boolean onTouch(View view1, MotionEvent event1) {
                                             popupWindow.dismiss();
                                             return true;
                                         }
