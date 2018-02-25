435df1b0a0b02b74dbecfec4575cbf46787f83ce
kpphiri <kphiri6@gatech.edu>
2018-02-24 20:36:09 -0500

    Created shelter model

---
+++ app/src/main/java/com/example/philipphiri/homelessapp/ShelterList.java
@@ -0,0 +1,35 @@
+package com.example.philipphiri.homelessapp;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+/**
+ * Created by philipphiri on 2/24/18.
+ */
+
+public class ShelterList {
+// extends ArrayAdapter<Shetler> {
+//    private Activity context;
+//    private List<Shelter> shelterList;
+//
+//    public ShetlerList(Activity context, List<Shelter> shelterList) {
+//        super(context, R.layout.activity_main, shelterList);
+//        this.context = context;
+//        this.shelterList = shelterList;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//        LayoutInflater inflater = context.getLayoutInflater();
+//        View listViewItem = inflater.inflate(R.layout.List.layout, null, true)''
+//        return super.getView(position, convertView, parent);
+//    }
+}
