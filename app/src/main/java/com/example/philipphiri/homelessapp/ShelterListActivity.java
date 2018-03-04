94e17e5bf33c63830a5303f908cb663df15cf9da
Bianca Guerrero <>
2018-03-04 18:22:23 -0500

    implemented shelter details popup

--- app/src/main/java/com/example/philipphiri/homelessapp/ShelterListActivity.java
+++ app/src/main/java/com/example/philipphiri/homelessapp/ShelterListActivity.java
@@ -9,10 +9,13 @@ import android.service.autofill.Dataset;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,6 +24,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -54,6 +58,13 @@ public class ShelterListActivity extends AppCompatActivity {
                 startActivity(new Intent(ShelterListActivity.this, WelcomePageActivity.class ));
             }
         });
+        listViewShelters.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Shelter s = (Shelter) listViewShelters.getItemAtPosition(position);
+                ShowDetails(v,s);
+            }
+        });
     }
 
     protected void onStart() {
@@ -62,7 +73,7 @@ public class ShelterListActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 shelters.clear();
-                //not going into for loop!
+
                 for (DataSnapshot tuple: dataSnapshot.getChildren()) {
                   Shelter shelter = new Shelter((String) tuple.child("Address").getValue(),(String) tuple.child("Capacity").getValue(), (String)tuple.child("Latitude ").getValue(),
                   (String) tuple.child("Longitude ").getValue(), (String)tuple.child("Phone Number").getValue(), (String) tuple.child("Restrictions").getValue(),
@@ -84,49 +95,35 @@ public class ShelterListActivity extends AppCompatActivity {
 
     }
 
-    //this will do the retrieving -- website way ??
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//        //attaching value event listener
-//        databaseShelters.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                //shelters.clear(); //?clearing previous shelter list
-//
-//                //iterating through all the nodes
-//                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
-//                    //getting shelter
-//                    Shelter shelter = postSnapshot.getValue(Shelter.class);
-//                    //adding shelter to the list
-//                    shelters.add(shelter);
-//                }
-//                //creating adapter
-//                ShelterList shelterAdapter = new ShelterList(ShelterListActivity.this, shelters);
-//                //attaching adapter to the listview
-//                listViewShelters.setAdapter(shelterAdapter);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        });
-//    }
-
     //for shelter details popup
-//    private void ShowDetails(View v) {
-//        TextView detailclose;
-//        myDialog.setContentView(R.layout.shelter_detail);
-//        detailclose = (TextView) myDialog.findViewById(R.id.detailclose);
-//        detailclose.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view) {
-//                myDialog.dismiss();
-//            }
-//        });
-//        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-//        myDialog.show();
-//
-//    }
+    public void ShowDetails(View v, Shelter s) {
+        TextView detailclose;
+        myDialog.setContentView(R.layout.shelter_detail);
+        detailclose = (TextView) myDialog.findViewById(R.id.detailclose);
+        detailclose.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        TextView tv1 = (TextView) myDialog.findViewById(R.id.shelterName);
+        tv1.setText(s.getShelterName());
+        TextView tv2 = (TextView) myDialog.findViewById(R.id.shelterCap);
+        tv2.setText(s.getShelterCapacity());
+        TextView tv3 = (TextView) myDialog.findViewById(R.id.shelterRes);
+        tv3.setText(s.getShelterRestrictions());
+        TextView tv4 = (TextView) myDialog.findViewById(R.id.shelterLongLat);
+        tv4.setText(s.getShelterLongitude() + " " + s.getShelterLatitude());
+        TextView tv5 = (TextView) myDialog.findViewById(R.id.shelterphone);
+        tv5.setText(s.getShelterPhone());
+        TextView tv6 = (TextView) myDialog.findViewById(R.id.shelteradd);
+        tv6.setText(s.getShelterAddress());
+        TextView tv7 = (TextView) myDialog.findViewById(R.id.shelternotes);
+        tv7.setText(s.getShelterNotes());
+
+        myDialog.show();
+
+    }
 }
