dfa556ae85f33de1520482f6f2badffe512b1ab5
Bianca Guerrero <>
2018-03-22 21:15:15 -0400

    set user profile attributes to specfic user's info in userProfileActivity, added user class methods, added release claims button ui and implementation for resetting those attributes

--- app/src/main/java/com/example/philipphiri/homelessapp/User.java
+++ app/src/main/java/com/example/philipphiri/homelessapp/User.java
@@ -7,18 +7,35 @@ package com.example.philipphiri.homelessapp;
 public class User {
     private String userType;
     private String permissionLevel;
+    private String residence;
+    private String name;
+    private String claims;
+    private String religion;
+    private String email;
 
-    public User(String userType, String permissionLevel) {
+    public User(String userType, String permissionLevel, String residence, String name, String claims) {
         this.userType = userType;
         this.permissionLevel = permissionLevel;
-
+        this.residence = residence;
+        this.name = name;
+        this.claims = claims;
+        //this.email = email;
+        //this.religion = religion;
     }
 
     public String getUserType(){
         return userType;
     }
 
+    public String getUserResidence() { return residence; }
 
+    public String getNumClaims() { return claims; }
+
+    public String getUserName() {return name; }
+
+    public String getUserEmail() { return email; }
+
+    public String getUserReligion() { return religion; }
 
     public String getPermissionLevel(){
         return permissionLevel;
--- app/src/main/java/com/example/philipphiri/homelessapp/UserProfileActivity.java
+++ app/src/main/java/com/example/philipphiri/homelessapp/UserProfileActivity.java
@@ -2,16 +2,23 @@ package com.example.philipphiri.homelessapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
 public class UserProfileActivity extends AppCompatActivity {
 
+    private Button claimsButton;
     private ImageView userPicture;
     private TextView userName;
     private TextView userEmail;
@@ -27,15 +34,52 @@ public class UserProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-        //set the values for the user
-        userPicture = findViewById(R.id.userImageView);
+        claimsButton = findViewById(R.id.releaseButton);
+        userPicture = findViewById(R.id.userImageView); //make button for image setting later
         userName = findViewById(R.id.nameTextView);
         userEmail = findViewById(R.id.emailTextView);
         residence = findViewById(R.id.residencyTextView);
-        curresidence = findViewById(R.id.residencySetTextView);
         claims = findViewById(R.id.numClaims);
+        curresidence = findViewById(R.id.residencySetTextView);
         numClaims = findViewById(R.id.claimsSetTextView);
-//        String user_id = user.getCurrentUser().getUid();
-//        DatabaseReference current_user = userData.child("Users").child(user_id);
+
+        //release claims and set residence back to None
+        user = FirebaseAuth.getInstance();
+        userData = FirebaseDatabase.getInstance().getReference().child("Users");
+
+
+        claimsButton.setOnClickListener(new View.OnClickListener() {
+            String user_id = user.getCurrentUser().getUid();
+            DatabaseReference current_user = userData.child(user_id);
+            @Override
+            public void onClick(View view) {
+                current_user.child("ShelterRegistered").setValue("none");
+                current_user.child("NumberClaimed").setValue("0");
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        String user_id = user.getCurrentUser().getUid();
+        final DatabaseReference current_user = userData.child(user_id);
+        current_user.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User u = new User((String)dataSnapshot.child("UserType").getValue(), (String)dataSnapshot.child("PermissionLevel").getValue(),
+                        (String)dataSnapshot.child("ShelterRegistered").getValue(), "meep", (String)dataSnapshot.child("NumberClaimed").getValue());
+                curresidence.setText(u.getUserResidence());
+                //set name
+                numClaims.setText(u.getNumClaims());
+                //set userEmail.setText(u.getEmail()); also add to constructor^
+                userName.setText(u.getUserName());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
